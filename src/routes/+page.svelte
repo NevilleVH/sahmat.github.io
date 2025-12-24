a0d4a945c1cf0674fc50f1c32f5657f05b22cd40
NevilleVH <1nevillevh@gmail.com>
2025-12-24 17:26:17 +0200

    Center board

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,88 +1,101 @@
 <script lang="ts">
 	import { type Board, dimension, newBoard, pieceImgs, type Piece, type Colour } from '$lib/pieces';
-	import {byEco, type Opening, ecoCodes} from "$lib/openings"
+	import { byEco, type Opening, ecoCodes } from '$lib/openings';
 	let board = $state<Board>(newBoard());
 
-    let turn = $state<Colour>("white")
+	let turn = $state<Colour>('white');
 	let selectedPiece = $state<Piece>();
 	let possibleMoves = $derived(selectedPiece?.possibleMoves(board));
 	// TODO: make move then show possible opening continuations/name
-	let selectedOpening = $state<Opening | undefined>(byEco.get(ecoCodes[0]))
-	let moveIdx = $state(-1)
+	let selectedOpening = $state<Opening | undefined>(byEco.get(ecoCodes[0]));
+	let moveIdx = $state(-1);
 </script>
 
-<div>
-	<select onchange={({currentTarget}) => {
-		const opening = byEco.get(currentTarget.value)
-		if (opening) {
-			selectedOpening = opening
-			board = newBoard()
-			moveIdx = -1
-		}
-	}}>
-		{#each byEco as [eco, {name}]}
-			<option value={eco}>{eco} {name}</option>
-		{/each}
-	</select>
-</div>
-<div>
-	<button disabled={moveIdx < 0} onclick={() => moveIdx--}>Prev</button>
-	<button disabled={!selectedOpening || moveIdx >= selectedOpening.uci.length-1 } onclick={() => {
-		moveIdx++
-		const move = selectedOpening?.uci.at(moveIdx)
-		if (!move) return;
-		const {start, end} = move
-		board[end.row][end.col] = board[start.row][start.col]
-		board[start.row][start.col] = null
-		}}>Next</button>
-</div>
-<div>{moveIdx} {selectedOpening?.uci}</div>
-<div id="board-container">
-<div id="board">
-	<div></div>
-	{#each { length: dimension } as _, i}
-	<div class="board-label">{String.fromCharCode(65+i)}</div>
-	{/each}
-	
-	{#each { length: dimension } as _, row}
-		<div class="board-label">{dimension - row}</div>
-		{#each { length: dimension } as _, col}
-			
-			{@const piece = board.at(row)?.at(col)}
-			<div class="square">
-				<button
-					
-					style="height:100%;width:100%"
-					style:background={possibleMoves?.find((p) => p.col===col && p.row === row) ? "lime" : (row + col) % 2 === 0 ? 'white' : 'rgba(0,0,0,0.5)'}
-					onclick={() => {
-                        // if (piece?.colour === turn) {
-                        //     selectedPiece = piece
-                        //     return
-                        // } 
-                        // if (selectedPiece && possibleMoves?.find((p) => p.col===col && p.row === row)) {
-                        //     board[selectedPiece.position.row][selectedPiece.position.col] = null
-                        //     selectedPiece.position = {row, col}
-                        //     board[row][col] = selectedPiece
-                        //     selectedPiece = undefined
-                        //     turn = turn === "black" ? 'white': "black"
-                        // }
-                    }}
-				>
-					{#if piece}
-						<img height="100%" width="100%" src={pieceImgs[piece.colour][piece.tag]} >
-					{/if}
-				</button>
+<div style="display:flex; flex-direction:column; height:80vh">
+	<div>
+		<select style:width="100%"
+			onchange={({ currentTarget }) => {
+				const opening = byEco.get(currentTarget.value);
+				if (opening) {
+					selectedOpening = opening;
+					board = newBoard();
+					moveIdx = -1;
+				}
+			}}
+		>
+			{#each byEco as [eco, { name }]}
+				<option value={eco}>{eco} {name}</option>
+			{/each}
+		</select>
+	</div>
+	<div style="display: flex; justify-content:center; gap:4px; margin:4px 0px">
+		<button disabled={moveIdx < 0} onclick={() => moveIdx--}>Prev</button>
+		<button
+			disabled={!selectedOpening || moveIdx >= selectedOpening.uci.length - 1}
+			onclick={() => {
+				moveIdx++;
+				const move = selectedOpening?.uci.at(moveIdx);
+				if (!move) return;
+				const { start, end } = move;
+				board[end.row][end.col] = board[start.row][start.col];
+				board[start.row][start.col] = null;
+			}}>Next</button
+		>
+	</div>
+	<div id="board-container">
+		<div id="board">
+			<div>
+				<!-- Empty div on top right -->
 			</div>
-		{/each}
-	{/each}
-</div>
+			{#each { length: dimension } as _, i}
+				<div class="board-label">{String.fromCharCode(65 + i)}</div>
+			{/each}
+
+			<div></div>
+
+			{#each { length: dimension } as _, row}
+				<div class="board-label">{dimension - row}</div>
+				{#each { length: dimension } as _, col}
+					{@const piece = board.at(row)?.at(col)}
+					<div class="square">
+						<button
+							style="height:100%;width:100%"
+							style:background={possibleMoves?.find((p) => p.col === col && p.row === row)
+								? 'lime'
+								: (row + col) % 2 === 0
+									? 'white'
+									: 'rgba(0,0,0,0.5)'}
+							onclick={() => {
+								// if (piece?.colour === turn) {
+								//     selectedPiece = piece
+								//     return
+								// }
+								// if (selectedPiece && possibleMoves?.find((p) => p.col===col && p.row === row)) {
+								//     board[selectedPiece.position.row][selectedPiece.position.col] = null
+								//     selectedPiece.position = {row, col}
+								//     board[row][col] = selectedPiece
+								//     selectedPiece = undefined
+								//     turn = turn === "black" ? 'white': "black"
+								// }
+							}}
+						>
+							{#if piece}
+								<img height="100%" width="100%" src={pieceImgs[piece.colour][piece.tag]} />
+							{/if}
+						</button>
+					</div>
+				{/each}
+				<div></div>
+			{/each}
+		</div>
+	</div>
 </div>
 
 <style>
 	#board {
 		display: grid;
-		grid-template-columns: repeat(9, 10vmin);
-		grid-template-rows: repeat(9, 10vmin);
+		grid-template-columns: repeat(10, 10vmin);
+		grid-template-rows: repeat(10, 10vmin);
 	}
 	.square {
 		border: 1px solid black;
@@ -94,5 +107,7 @@
 	#board-container {
 		display: flex;
 		justify-content: center;
+		align-items: center;
+		flex-grow: 1;
 	}
 </style>
